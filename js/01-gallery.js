21d2eed97d25437764acdcf4a3b72bbf9481d613
oleg-mahnichev <124716395+oleg-mahnichev@users.noreply.github.com>
2023-06-27 20:16:55 +0200

    finish_first_part_HW7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+function createGalleryMarkup() {
+    const galleryList = document.querySelector('.gallery');
+
+    const galleryMarkup = galleryItems
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+           </a>
+         </li>`
+        )
+        .join('');
+
+    galleryList.innerHTML = galleryMarkup;
+}
+
+// Додавання прослуховувача на клік по елементу галереї
+document.querySelector('.gallery').addEventListener('click', openModal);
+
+// Відкриття модального вікна по кліку на елементі галереї
+function openModal(event) {
+    event.preventDefault();
+
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const largeImageURL = event.target.parentNode.getAttribute('href');
+    const instance = basicLightbox.create(`<img src="${largeImageURL}" />`, {
+        onClose: () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        },
+    });
+
+    instance.show();
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    function handleKeyPress(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    }
+}
+
+// Виклик функції для створення галереї
+createGalleryMarkup();
